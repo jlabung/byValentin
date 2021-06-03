656cefa93dc82e7727e30725b569566c4ea6386c
Susan <khatri.susan123@gmail.com>
2021-06-03 15:28:53 +0200

    styled mediaQuery and modal

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ function init() {
   fetch("https://byvalentin.skmmd.dk/wp-json/wp/v2/products?_embed")
     .then((initial) => initial.json())
     .then(callBack);
-
- 
 }
 
 function callBack(data) {
@@ -22,32 +20,29 @@ function showService(singleService) {
   const template = document.querySelector("template").content;
   const clone = template.cloneNode(true);
   const img_url = singleService._embedded["wp:featuredmedia"][0].source_url;
-  clone.querySelector("h1").textContent = singleService.title.rendered;
-  clone.querySelector("h2").innerHTML = singleService.price;
+  clone.querySelector(".name").textContent = singleService.title.rendered;
+  clone.querySelector(".price").innerHTML = singleService.price;
+  clone.querySelector(".sdesc").innerHTML = singleService.shortdescription;
   clone.querySelector(".product-img").src = img_url;
 
   clone.querySelector("button").addEventListener("click", () => {
-    
+    const modal = document.querySelector(".modal-background");
+    const img_urlTwo =
+      singleService._embedded["wp:featuredmedia"][0].source_url;
+    modal.classList.remove("hide");
+    modal.querySelector("h3").textContent = singleService.title.rendered;
+    modal.querySelector("p").textContent = singleService.longdescription;
+    modal.querySelector(".modal-image").src = img_urlTwo;
 
-      const modal = document.querySelector(".modal-background");
-      const img_urlTwo = singleService._embedded["wp:featuredmedia"][0].source_url;
-      modal.classList.remove("hide");
-      modal.querySelector("h3").textContent = singleService.title.rendered;
-      modal.querySelector("p").textContent= singleService.shortdescription;
-      modal.querySelector(".modal-image").src= img_urlTwo;
-
-      const elmaint = document.querySelector(".cont");
-  elmaint.appendChild(clone);
-
+    const elmaint = document.querySelector(".cont");
+    elmaint.appendChild(clone);
   });
 
   const elmain = document.querySelector(".content");
   elmain.appendChild(clone);
 }
 
-
 const modal = document.querySelector(".modal-background");
 modal.addEventListener("click", () => {
   modal.classList.add("hide");
 });
-
